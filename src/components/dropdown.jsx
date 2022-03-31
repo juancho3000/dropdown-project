47edc00dccbfd690ba2532add8762f19b5e76ca4
juan-suarez <jsuarez0698@gmail.com>
2022-03-31 10:58:18 -0400

    btn function

--- src/components/dropdown.jsx
+++ src/components/dropdown.jsx
@@ -1,23 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import {TiArrowSortedDown} from 'react-icons/ti';
 
 function Dropdown (){
+    const [itsOn, setItsOn] = useState(false);
     return(
         <div className="select">
-            <div className="select-btn">Click here <TiArrowSortedDown/> </div>
-            <div className="select-content">
-                <div className="select-item">
-                    nancy
+            <div className="select-btn" onClick={(e) => 
+            setItsOn(!itsOn)}>
+                Click here 
+                <div className="arrow">
+                <TiArrowSortedDown/> 
                 </div>
-                <div className="select-item">
-                    miguel
                 </div>
-                <div className="select-item">
-                    guillermo
+                <span className="under"></span>
+                {itsOn && ( 
+                    <div className="select-content">
+                    <div className="select-item">
+                        nancy
+                    </div>
+                    <div className="select-item">
+                        miguel
+                    </div>
+                    <div className="select-item">
+                        guillermo
+                    </div>
                 </div>
-            </div>
+                )}
         </div>
-    )
+    );
 }
-
 export default Dropdown;
